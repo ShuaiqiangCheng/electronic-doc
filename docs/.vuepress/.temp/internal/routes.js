71b629a3eb864efa62cfe985194fceb54d006876
Lan <biubiusmsklxq@outlook.com>
2025-03-12 16:21:24 +0800

    Initial commit

---
+++ docs/.vuepress/.temp/internal/routes.js
@@ -0,0 +1,7 @@
+export const redirects = JSON.parse("{}")
+
+export const routes = Object.fromEntries([
+  ["/get-started.html", { loader: () => import(/* webpackChunkName: "get-started.html" */"D:/VSWorkSpace/vuepress-starter/docs/.vuepress/.temp/pages/get-started.html.js"), meta: {"title":"Get Started"} }],
+  ["/", { loader: () => import(/* webpackChunkName: "index.html" */"D:/VSWorkSpace/vuepress-starter/docs/.vuepress/.temp/pages/index.html.js"), meta: {"title":"Home"} }],
+  ["/404.html", { loader: () => import(/* webpackChunkName: "404.html" */"D:/VSWorkSpace/vuepress-starter/docs/.vuepress/.temp/pages/404.html.js"), meta: {"title":""} }],
+]);
